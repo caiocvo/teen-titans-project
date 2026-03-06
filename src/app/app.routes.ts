180735935bd195ae9083ab25749702e7ebb33f8e
caiocvo <seza45670@gmail.com>
2026-03-06 17:07:59 -0300

    create components and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'navbar',
+    component: Navbar,
+  },
+];
