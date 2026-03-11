d5c1e02a8efa22cd86f7b187d541ddd06e2c95ce
caiocvo <seza45670@gmail.com>
2026-03-11 19:51:38 -0300

    alterate text

--- src/app/components/initial/initial.ts
+++ src/app/components/initial/initial.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-initial',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './initial.html',
   styleUrl: './initial.scss',
 })
-export class Initial {}
+export class Initial {
+  title: string = 'HERÓIS DE JUMP CITY';
+  paragraph: string = 'Passe o mouse sobre um Titã para conhecer melhor cada herói.';
+
+  defaultTitle: string = this.title;
+  defaultParagraph: string = this.paragraph;
+
+  changeText(title: string, paragraph: string) {
+    this.title = title;
+    this.paragraph = paragraph;
+  }
+
+  resetText() {
+    this.title = this.defaultTitle;
+    this.paragraph = this.defaultParagraph;
+  }
+}
