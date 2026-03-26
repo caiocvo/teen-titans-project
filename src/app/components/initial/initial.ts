586e4c3ee0fa764e9692f5f9ef1fcd372c6a84df
caiocvo <seza45670@gmail.com>
2026-03-26 17:36:28 -0300

    feat: update characters screen layout and styling

--- src/app/components/initial/initial.ts
+++ src/app/components/initial/initial.ts
@@ -8,19 +8,67 @@ import { Component, Input } from '@angular/core';
   styleUrl: './initial.scss',
 })
 export class Initial {
-  title: string = 'HERÓIS DE JUMP CITY';
-  paragraph: string = 'Passe o mouse sobre um Titã para conhecer melhor cada herói.';
-
-  defaultTitle: string = this.title;
-  defaultParagraph: string = this.paragraph;
-
-  changeText(title: string, paragraph: string) {
-    this.title = title;
-    this.paragraph = paragraph;
-  }
-
-  resetText() {
-    this.title = this.defaultTitle;
-    this.paragraph = this.defaultParagraph;
-  }
+  personagens = [
+    //ESTELAR
+    {
+      nome: 'Estelar',
+      descricao: 'Uma princesa alienígena com poderes incríveis.',
+      img: '/backgrounds/characters/starfire.png',
+      imgSombra: 'backgrounds/shadows/sombra.estelar.png',
+      corPrincipal: '#D84800',
+      corSecundaria: '#6E1F00',
+      width: 520,
+      bottom: -420,
+      right: 20,
+    },
+    //RAVENA
+    {
+      nome: 'Ravena',
+      descricao: 'Uma poderosa empata com habilidades místicas.',
+      img: '/backgrounds/characters/raven.webp',
+      imgSombra: '/backgrounds/shadows/sombra.ravena.png',
+      corPrincipal: '#0D0055',
+      corSecundaria: '#05001F',
+      width: 520,
+      bottom: 0,
+      right: 30,
+    },
+    {
+      //ROBIN
+      nome: 'Robin',
+      descricao: 'O líder estratégico dos Titãs e mestre em combate.',
+      img: '/backgrounds/characters/robin.webp',
+      imgSombra: '/backgrounds/shadows/sombra.robin.png',
+      corPrincipal: '#5D0000',
+      corSecundaria: '#420000',
+      width: 600,
+      bottom: 200,
+      right: 100,
+    },
+    //MUTANO
+    {
+      nome: 'Beastboy',
+      descricao: 'Um metamorfo habilidoso que se transforma em animais.',
+      img: '/backgrounds/characters/beastboy.webp',
+      imgSombra: '/backgrounds/shadows/sombra.mutano.png',
+      corPrincipal: '#0B4400',
+      corSecundaria: '#051C00',
+      width: 520,
+      bottom: 0,
+      right: 80,
+    },
+    //CYBORG
+    {
+      nome: 'Cyborg',
+      descricao: 'Um homem meio máquina meio humano com um grande coração.',
+      img: '/backgrounds/characters/cyborg.webp',
+      imgSombra: '/backgrounds/shadows/sombra.cyborg.png',
+      corPrincipal: '#004798',
+      corSecundaria: '#003A7C',
+      width: 520,
+      bottom: 0,
+      right: 90,
+    },
+  ];
+  personagemAtivo = this.personagens[2]; //Robin default
 }
