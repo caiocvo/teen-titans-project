01f9206be8d01894c815cf02823673eefe46fcf2
caiocvo <seza45670@gmail.com>
2026-05-11 19:24:38 -0300

    style: polish character overview section

--- src/app/components/nickname/nickname.ts
+++ src/app/components/nickname/nickname.ts
@@ -7,18 +7,33 @@ import { Component, ChangeDetectorRef } from '@angular/core';
   styleUrl: './nickname.scss',
 })
 export class Nickname {
-  nomesTitans = ['Estelar', 'Ravena', 'Robin', 'Mutano', 'Cyborg'];
+  titans = [
+    {
+      name: 'Estelar',
+      iconUrl: '/backgrounds/icons/starfire-icon.png',
+      iconColor: '#ef6d2c',
+    },
+    { name: 'Ravena', iconUrl: '/backgrounds/icons/raven-icon.png', iconColor: '#480aa5' },
+
+    { name: 'Robin', iconUrl: '/backgrounds/icons/robin-icon.png', iconColor: '#af1414' },
+    {
+      name: 'Mutano',
+      iconUrl: '/backgrounds/icons/beastboy-icon.png',
+      iconColor: '#40ba25',
+    },
+    { name: 'Cyborg', iconUrl: '/backgrounds/icons/cyborg-icon.png', iconColor: '#317cd2' },
+  ];
 
   constructor(
     private characterStateService: CharacterStateService,
     private cdr: ChangeDetectorRef,
   ) {}
 
-  name: any;
+  titanNick: any;
 
   ngOnInit() {
     this.characterStateService.idActual$.subscribe((index) => {
-      this.name = this.nomesTitans[index];
+      this.titanNick = this.titans[index];
       this.cdr.detectChanges();
     });
   }
