74f20b98052ed461719e7bd7fb2969fa709d5080
caiocvo <seza45670@gmail.com>
2026-05-11 20:18:13 -0300

    fix: update character api route

--- src/app/services/information/information-service.ts
+++ src/app/services/information/information-service.ts
@@ -5,14 +5,14 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class InformationService {
-  private api = 'http://localhost:3001/';
+  private api = 'https://teen-titans-project.onrender.com/char';
 
   constructor(private http: HttpClient) {}
 
   getCharById(id: number) {
-    return this.http.get<Character>(`${this.api}char/${id}`);
+    return this.http.get<Character>(`${this.api}/${id}`);
   }
   getChars() {
-    return this.http.get<Character[]>(`${this.api}char`);
+    return this.http.get<Character[]>(`${this.api}`);
   }
 }
