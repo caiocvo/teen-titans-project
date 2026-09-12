22cd92c2bc1ad4972345e63574870fa9e6ed16c2
caiocvo <seza45670@gmail.com>
2026-09-12 10:12:36 -0300

    style: add responsiveness to titans component

--- src/app/components/titans/titans.ts
+++ src/app/components/titans/titans.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-titans',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './titans.html',
   styleUrl: './titans.scss',
 })
